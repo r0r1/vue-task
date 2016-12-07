6ad98a6cd6c52e8e975d8b68c5855f594978e235
Rori Kurniadi <rori.kurniadi@yahoo.com>
2016-12-08 03:20:07 +0700

    layout admin lte

---
+++ src/components/layouts/Content.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>Vue Task App</h1>
+      </section>
+      <!-- Main content -->
+      <section class="content">
+        <transition>
+          <keep-alive>
+            <router-view></router-view>
+          </keep-alive>
+        </transition>
+      </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</template>
+
+<script>
+  export default {
+    name: 'Content',
+    data() {
+      return {
+      };
+    },
+  };
+</script>
---
+++ src/components/layouts/Sidebar.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="~assets/images/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+            <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+            </button>
+          </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview" v-for="row in menus">
+          <router-link :to="row.link">
+            <i :class="row.icon"></i> 
+            <span>{{ row.name }}</span> 
+          </router-link>
+          
+          <ul v-show="row.hasOwnProperty('child') && typeof row.child.length != 'undefined'" class="treeview-menu">
+            <li v-for="child in row.child">
+              <router-link :to="child.link">
+                <i :class="child.icon"></i> 
+                <span>{{ child.name }}</span> 
+              </router-link>
+            </li>
+          </ul> 
+        </li>
+
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar',
+    data() {
+      return {
+        menus: this.$parent.$data.menus || [],
+      };
+    },
+    ready() {
+    },
+  };
+</script>
