a985c9b73b46c59a1dd07b585a30829594b0569a
Rori <rori.kurniadi@yahoo.com>
2016-12-10 19:49:00 +0700

    add vuetable & vue-resource

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationDropdown from 'vuetable-2/src/components/VuetablePaginationDropdown';
 import App from './App';
 import Router from './routes';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.component('vuetable', Vuetable);
+Vue.component('vuetable-pagination', VuetablePagination);
+Vue.component('vuetable-pagination-dropdown', VuetablePaginationDropdown);
 
 const router = new VueRouter({
   linkActiveClass: 'active',
