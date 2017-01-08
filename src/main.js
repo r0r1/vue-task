a76895e57d8bf2f676c29859a433cfd500707dbd
Rori Kurniadi <rori.kurniadi@yahoo.com>
2017-01-08 19:06:02 +0700

    remove vue-table

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
-import Vuetable from 'vuetable-2/src/components/Vuetable';
-import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
-import VuetablePaginationDropdown from 'vuetable-2/src/components/VuetablePaginationDropdown';
 import App from './App';
 import Router from './routes';
 import AuthService from './services/auth';
@@ -11,10 +8,6 @@ import store from './store';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-
-Vue.component('vuetable', Vuetable);
-Vue.component('vuetable-pagination', VuetablePagination);
-Vue.component('vuetable-pagination-dropdown', VuetablePaginationDropdown);
 
 const router = new VueRouter({
   linkActiveClass: 'active',
@@ -49,4 +42,3 @@ new Vue({
   store,
   components: { App },
 });
-
