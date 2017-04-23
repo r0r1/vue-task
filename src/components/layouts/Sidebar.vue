f7eb26af0f479fcbc5c5352b815d5e4a09af7619
Rori Kurniadi <rori.kurniadi@yahoo.com>
2017-04-23 15:41:50 +0700

    remove use info on Sidebar comp

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -3,16 +3,6 @@
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="~assets/images/avatar.png" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p>{{ currentUser.name }}</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
       <!-- search form -->
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
